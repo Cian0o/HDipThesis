5af576e896861f1e8830c4d40e767b2362253720
cianwalker <10021419@mydbs.ie>
2022-11-02 21:10:00 +0000

    Push for the sake of documantation backup.

---
+++ app.js
@@ -0,0 +1,14 @@
+const loginIcon = document.querySelector('#loginIcon');
+
+loadEventListeners();
+
+function loadEventListeners(){
+    loginIcon.addEventListener(click, logInForm );
+}
+
+function logInForm{
+
+
+}
+
+//Write prescription submission with rendering of table into UI along with a clear and confirm/submit button!
