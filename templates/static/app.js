36c5aca6ad62b3df38eedbb0fdd5fee4c33a1f72
cianwalker <10021419@mydbs.ie>
2022-11-13 10:39:43 +0000

    First pass at prototype form with decent animated button  :)

---
+++ templates/static/app.js
@@ -0,0 +1,25 @@
+const loginIcon = document.querySelector('#loginIcon');
+
+function loginCredentials(){
+    var x = document.getElementById("login-content");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+loadEventListeners();
+
+function loadEventListeners(){
+    loginIcon.addEventListener(click, logInForm );
+}
+
+
+
+function logInForm{
+
+
+}
+
+//Write prescription submission with rendering of table into UI along with a clear and confirm/submit button!
