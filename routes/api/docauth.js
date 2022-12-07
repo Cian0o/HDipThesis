38f74440b95de5a59759236d97e3b87515c68463
cianwalker <10021419@mydbs.ie>
2022-12-07 14:19:32 +0000

    Authentication now returning User profiles minus passwords

--- routes/api/docauth.js
+++ routes/api/docauth.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const docauth = require('../../middleware/docauth')
+const Surgery = require('../../models/Surgery');
 const router = express.Router();
 
 // route is: GET api/docauth
 // Description: Test Route
 // Access: Public
-router.get('/', docauth, (req, res) => res.send('Doctor Authd!'));
+router.get('/', docauth, async (req, res) => {
+    try{
+        const surgery = await Surgery.findById(req.surgery.id).select('-password');
+        res.json(surgery);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
--- routes/api/pharmaauth.js
+++ routes/api/pharmaauth.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const pharmaauth = require('../../middleware/pharmaauth')
+const Pharmacy = require('../../models/Pharmacy');
 const router = express.Router();
 
 // route is: GET api/docauth
 // Description: Test Route
 // Access: Public
-router.get('/', pharmaauth, (req, res) => res.send('Pharma Authd!'));
+router.get('/', pharmaauth, async (req, res) => {
+    try{
+        const pharmacy = await Pharmacy.findById(req.pharmacy.id).select('-password');
+        res.json(pharmacy);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
