eb83f4cb13e61add4a61fd63485a9352b8e2df29
cianwalker <10021419@mydbs.ie>
2022-12-08 00:02:49 +0000

    And same again for Pharma login

--- routes/api/pharmaauth.js
+++ routes/api/pharmaauth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const pharmaauth = require('../../middleware/pharmaauth')
-const Pharmacy = require('../../models/Pharmacy');
+const {check, validationResult} = require("express-validator");
+const Pharmacy = require("../../models/Pharmacy");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const router = express.Router();
 
 // route is: GET api/docauth
@@ -15,5 +19,73 @@ router.get('/', pharmaauth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+//Route: POST to api/auth
+//Authenticates users and gets token
+//Access: Public
+
+router.post('/', [
+        check('email', 'Please Enter a registered Email').isEmail().not().isEmpty(),
+        check('password', 'Please enter a valid password').exists()],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+        console.log(req.body);
+
+
+        const {email, password} = req.body;
+
+        try{
+            let pharmacy = await Pharmacy.findOne({email});
+
+            //Check is  user exists
+
+            if(!pharmacy){
+                res.status(400).json({errors: [{msg: 'Invalid Credentials'}]});
+            }
+
+
+            const isMatch = await bcrypt.compare(password,pharmacy.PharmaPassword);
+
+            if(!isMatch){
+                res.status(400).json({errors: [{msg: 'Invalid Credentials'}]});
+            }
+
+
+
+
+            //Return JWT
+            const payload = {
+                pharmacy:{
+                    id: pharmacy.id
+                }
+            }
+            jwt.sign(
+                payload,
+                config.get('jwtSecretPharma'),
+                {expiresIn: 360000},
+                (err, token)=> {
+                    if(err) throw err;
+                    res.json({token})
+
+                });
+
+
+
+
+        } catch(err){
+            console.error(err.message);
+            res.status(500).send("Server Error");
+
+        }
+
+
+
+
+
+
+    });
 
 module.exports = router;
